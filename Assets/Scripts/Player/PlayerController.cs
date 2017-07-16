c7dbef243bbc6161edf94349192d97817ee5ac15
Joshua Maciak <joshua.maciak@gmail.com>
2017-07-16 19:04:26 -0400

    added primitive player controller to control movement via WASD keys

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private PlayerMovement playerMovement;
+	void Start () {
+		playerMovement = GetComponent<PlayerMovement>();
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+			playerMovement.StartMovingNorth ();
+		} else if (Input.GetKeyDown(KeyCode.S)) {
+			playerMovement.StartMovingSouth();
+		} else if (Input.GetKeyDown(KeyCode.D)) {
+			playerMovement.StartMovingEast();
+		} else if (Input.GetKeyDown(KeyCode.A)) {
+			playerMovement.StartMovingWest();
+		} else {
+			playerMovement.StopMoving();
+		}
+	}
+}
