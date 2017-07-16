d6d66d2adcf96ee721f933b77e28e2390c5c8f0e
Joshua Maciak <joshua.maciak@gmail.com>
2017-07-15 22:16:13 -0400

    wrapper for Unity's Debug.Log(...)

---
+++ Assets/Scripts/JLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ *  A wrapper for Unity's Debug.Log(...). Since Unity provides no simple
+ *  way to silence logs (e.g. for release) we need this class.
+**/
+public class JLogger {
+	public static bool ENABLED; // set to true if you want to log to the console
+	public static void Log(object message) {
+		if (ENABLED) {
+			Debug.Log (message);
+		}
+	}
+}
