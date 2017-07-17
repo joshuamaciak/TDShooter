3d88f5728d4f732047e1a39ace82251e3fc19961
Joshua Maciak <joshua.maciak@gmail.com>
2017-07-16 22:26:24 -0400

    fixed bug that moved player only when key was down during frame

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,22 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	private PlayerMovement playerMovement;
+	private PlayerMovement playerMovement; // the PlayerMovement object attached to the player
+	#if UNITY_EDITOR
+	private KeyCode lastKeyDown; // the last key that was pressed down
+	#endif
+
 	void Start () {
 		playerMovement = GetComponent<PlayerMovement>();
 	}
 	
 	void Update () {
+		#if UNITY_EDITOR
 		if (Input.GetKeyDown(KeyCode.W)) {
+			lastKeyDown = KeyCode.W;
 			playerMovement.StartMovingNorth ();
-		} else if (Input.GetKeyDown(KeyCode.S)) {
-			playerMovement.StartMovingSouth();
-		} else if (Input.GetKeyDown(KeyCode.D)) {
-			playerMovement.StartMovingEast();
-		} else if (Input.GetKeyDown(KeyCode.A)) {
-			playerMovement.StartMovingWest();
-		} else {
-			playerMovement.StopMoving();
 		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			lastKeyDown = KeyCode.S;
+			playerMovement.StartMovingSouth();
+		} 
+		if (Input.GetKeyDown(KeyCode.D)) {
+			lastKeyDown = KeyCode.D;
+			playerMovement.StartMovingEast();
+		} 
+		if (Input.GetKeyDown(KeyCode.A)) {
+			lastKeyDown = KeyCode.A;
+			playerMovement.StartMovingWest();
+		} 
+
+		if (Input.GetKeyUp (lastKeyDown)) {
+			playerMovement.StopMoving ();
+		}
+		#endif
 	}
 }
