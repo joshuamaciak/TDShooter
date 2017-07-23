c8f87125fe0f5f2d79ac85ba534b2764a6cbc0e3
Joshua Maciak <joshua.maciak@gmail.com>
2017-07-23 13:22:43 -0400

    added object pooling to bullets

---
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+	public float maxSecondsAlive; // the amount of time the bullet will live
+	private float secondsAliveCounter; // the amount of time the bullet has been alive 
+
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GetComponent<Poolable>().objectPool.ReturnObject(gameObject);
+	}
+	void Update() {
+		secondsAliveCounter += Time.deltaTime;
+		if(secondsAliveCounter >= maxSecondsAlive) {
+			secondsAliveCounter = 0;	// reset object state
+			GetComponent<Poolable>().objectPool.ReturnObject(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	public int id;				// unique id
+	public string name; 		// name
+	public float damage;		// amount of damage one bullet inflicts
+	public float rateOfFire; 	// the rate of fire of the weapon (in bullets/second)
+	public float recoilAmount;  // the amount of force a bullet inflicts on the player
+	public float range;			// the distance the bullet will travel
+	public float bulletSpeed; 	// the speed of the bullet
+	public int weight;			// the weight of the weapon [1,3] (heavier = slower player)
+	
+	//private GameObject bullet;
+	private ObjectPool bulletPool;
+	void Start() {
+		bulletPool = GetComponent<ObjectPool>();
+	}
+	public void Fire() {
+		JLogger.Log("bang!");
+		GameObject newBullet = bulletPool.GetObject();
+		newBullet.transform.position    = transform.position;
+		newBullet.transform.rotation    = transform.rotation;
+		newBullet.GetComponent<Rigidbody>().velocity = newBullet.transform.forward * bulletSpeed;
+		// todo implement code that spawns bullet, etc...
+
+	}
+}
